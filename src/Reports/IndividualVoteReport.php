ba6f22c94f8c71f69f1d8ec8405cf676dd2a701c
Jarrad Littleford <jarrad.littleford@protonmail.com>
2016-11-02 22:48:13 +1030

    Individual report

--- src/Reports/IndividualVoteReport.php
+++ src/Reports/IndividualVoteReport.php
@@ -8,14 +8,28 @@ use Soda\Voting\Models\Nominee;
 use Soda\Voting\Models\Vote;
 use Zofe\Rapyd\DataFilter\DataFilter;
 use Zofe\Rapyd\DataGrid\DataGrid;
+use Carbon\Carbon;
 
 class IndividualVoteReport extends AbstractReport{
     public function export(){
-        return 'Not yet implemented';
+        $grid = DataGrid::source(Vote::with('user', 'nominee', 'nominee.category'));
+        $grid->add('id', 'ID', true);
+        $grid->add('nominee.name', 'Nominee Name', true);
+        $grid->add('nominee.category.name', 'Nominee Category');
+        $grid->add('user.username','User Name', true);
+        $grid->add('user.email', 'User email', true);
+        $grid->add('ip_address', 'IP Address');
+        $grid->add('created_at', 'Time', true)->cell(function($value){
+            return Carbon::parse($value)->timezone('Australia/Sydney')->toDayDateTimeString();
+        });
+
+        $as_excel = ['delimiter'=>',', 'enclosure'=>'"', 'line_ending'=>"\n"];
+        return $grid->buildCSV('individuial_vote', 'Y-m-d', true, $as_excel);
+
     }
 
     public function view(){
-        $filter = DataFilter::source(Vote::with('user', 'nominee'));
+        $filter = DataFilter::source(Vote::with('user', 'nominee', 'nominee.category'));
         $filter->add('nominee.name','Nominee Name', 'text');
         $filter->add('user.username', 'User Name', 'text');
         $filter->add('ip_address', 'IP Address', 'text');
@@ -26,9 +40,13 @@ class IndividualVoteReport extends AbstractReport{
         $grid = DataGrid::source($filter);
         $grid->add('id', 'ID', true);
         $grid->add('nominee.name', 'Nominee Name', true);
+        $grid->add('nominee.category.name', 'Nominee Category');
         $grid->add('user.username','User Name', true);
         $grid->add('user.email', 'User email', true);
         $grid->add('ip_address', 'IP Address');
+        $grid->add('created_at', 'Time', true)->cell(function($value){
+            return Carbon::parse($value)->timezone('Australia/Sydney')->toDayDateTimeString();
+        });
 
         $grid->paginate(20);
 
